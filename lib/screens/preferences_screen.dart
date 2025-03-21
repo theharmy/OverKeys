78afab88b92cc73838b6ebfd86b0909e68b39960
Angelo Convento <convento.angelo@gmail.com>
2025-03-21 18:36:46 +0800

    chore: bump version to 0.2.3-alpha.1

--- lib/screens/preferences_screen.dart
+++ lib/screens/preferences_screen.dart
@@ -697,7 +697,7 @@ class _PreferencesScreenState extends State<PreferencesScreen>
                 fontSize: 24,
                 fontWeight: FontWeight.bold)),
         const SizedBox(height: 8),
-        Text('Version 0.2.2',
+        Text('Version 0.2.3-alpha.1',
             style: TextStyle(color: colorScheme.onSurface.withAlpha(153))),
         const SizedBox(height: 16),
         Text(
