7bdc35c3b3be68ce9e2d30f3807657e2f9b05ad0
Angelo Convento <convento.angelo@gmail.com>
2025-03-30 13:55:29 +0800

    chore: remove unused import

--- lib/screens/preferences_screen.dart
+++ lib/screens/preferences_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:desktop_multi_window/desktop_multi_window.dart';
 import 'package:flutter/services.dart';
